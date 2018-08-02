bb720382698324b52403b6fd208d71eb366837cd
Nikita Ivanov <ramzesxi@gmail.com>
2018-08-02 21:07:14 +0300

    add google benchmark

--- main.cpp
+++ main.cpp
@@ -3,36 +3,38 @@
 #include "instruction_sets/avx/operations.h"
 #include "instruction_sets/avx-512/operations.h"
 
+#include <benchmark/benchmark.h>
+
 #include <cstddef>
 #include <iostream>
 #include <chrono>
 
-template <typename T>
-void benchmark(T func)
+void BM_accumulate_base(benchmark::State& state)
 {
-    volatile float initial = 0;
-
-    const auto start = std::chrono::high_resolution_clock::now();
-    const auto result = func(initial);
-    const auto end = std::chrono::high_resolution_clock::now();
-
-    const auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-    std::cout
-        << "\tanswer: " << result
-        << "\ttime: " << duration.count()
-        << std::endl;
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(simd_none::accumulate(0));
+    }
 }
 
-int main()
+void BM_accumulate_sse(benchmark::State& state)
 {
-    std::cout << "none:\n";
-    benchmark(simd_none::accumulate);
-
-    std::cout << "sse:\n";
-    benchmark(simd_sse::accumulate);
-
-    std::cout << "avx:\n";
-    benchmark(simd_avx::accumulate);
-
-    return 0;
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(simd_sse::accumulate(0));
+    }
 }
+
+void BM_accumulate_avx(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(simd_avx::accumulate(0));
+    }
+}
+
+BENCHMARK(BM_accumulate_base);
+BENCHMARK(BM_accumulate_sse);
+BENCHMARK(BM_accumulate_avx);
+
+BENCHMARK_MAIN();
