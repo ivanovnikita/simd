65de991a2ac8a3de6e3b46115ca6199f7c27aa6d
Nikita Ivanov <ramzesxi@gmail.com>
2018-08-03 11:31:09 +0300

    add aligned allocators

---
+++ dynamic_aligned_allocator.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <xmmintrin.h>
+
+#include <cstdint>
+#include <cstddef>
+#include <stdexcept>
+
+namespace simd
+{
+    template <typename T>
+    class dynamic_aligned_allocator
+    {
+    public:
+        using value_type = T;
+        using pointer = T*;
+        using size_type = size_t;
+
+        explicit dynamic_aligned_allocator(uint8_t alignment) noexcept;
+
+        pointer allocate(size_type);
+        void deallocate(pointer, size_type) noexcept;
+
+    private:
+        uint8_t m_alignment;
+    };
+
+    template <typename T>
+    dynamic_aligned_allocator<T>::dynamic_aligned_allocator(uint8_t alignment) noexcept
+        : m_alignment(alignment)
+    {
+    }
+
+    template <typename T>
+    typename dynamic_aligned_allocator<T>::pointer dynamic_aligned_allocator<T>::allocate(size_type n)
+    {
+        auto ptr = reinterpret_cast<pointer>(_mm_malloc(sizeof(T) * n, m_alignment));
+        if (ptr == nullptr)
+        {
+            throw std::bad_alloc();
+        }
+        return ptr;
+    }
+
+    template <typename T>
+    void dynamic_aligned_allocator<T>::deallocate(pointer ptr, size_type) noexcept
+    {
+        _mm_free(ptr);
+    }
+}
---
+++ static_aligned_allocator.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <xmmintrin.h>
+
+#include <cstdint>
+#include <cstddef>
+#include <stdexcept>
+
+namespace simd
+{
+    template <typename T, int N>
+    class static_aligned_allocator
+    {
+    public:
+        using value_type = T;
+        using pointer = T*;
+        using size_type = size_t;
+
+        pointer allocate(size_type);
+        void deallocate(pointer, size_type) noexcept;
+    };
+
+    template <typename T, int N>
+    typename static_aligned_allocator<T, N>::pointer static_aligned_allocator<T, N>::allocate(size_type n)
+    {
+        auto ptr = reinterpret_cast<pointer>(_mm_malloc(sizeof(T) * n, N));
+        if (ptr == nullptr)
+        {
+            throw std::bad_alloc();
+        }
+        return ptr;
+    }
+
+    template <typename T, int N>
+    void static_aligned_allocator<T, N>::deallocate(pointer ptr, size_type) noexcept
+    {
+        _mm_free(ptr);
+    }
+}
