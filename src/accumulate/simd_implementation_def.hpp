9adfc8b0e2d40bc4e81b99a11e77d0c7a5d6e407
Nikita Ivanov <ramzesxi@gmail.com>
2018-09-12 15:48:00 +0300

    rewrite accumulate algorithm with partial_load and horizontal_add

--- src/accumulate/simd_implementation_def.hpp
+++ src/accumulate/simd_implementation_def.hpp
@@ -22,33 +22,35 @@ namespace simd::detail
         constexpr size_t substep = vector<T, simd_tag>::capacity;
         const size_t step = substep * adds_per_cycle;
         size_t i = step;
+        const T* values_ptr = nullptr;
         for (; i < values.size(); i += step)
         {
-            const T* values_ptr = &values[i - step];
+            values_ptr = &values[i - step];
             for (size_t j = 0; j < adds_per_cycle; ++j, values_ptr += substep)
             {
                 simd_results[j] += vector<T, simd_tag>(values_ptr);
             }
         }
 
-        for (size_t i = 1; i < adds_per_cycle; ++i)
+        for (size_t r = 1; r < adds_per_cycle; ++r)
         {
-            simd_results[0] += simd_results[i];
+            simd_results[0] += simd_results[r];
         }
 
-        i -= step;
+        i = i - step + substep;
+        values_ptr = &values[i - substep];
+        for (; i < values.size(); i += substep, values_ptr += substep)
+        {
+            simd_results[0] += vector<T, simd_tag>(values_ptr);
+        }
+
+        i -= substep;
+        vector<T, simd_tag> remaining;
+        remaining.load_partial(&values[i], static_cast<uint8_t>(values.size() - i));
+        simd_results[0] += remaining;
+
         T result = 0;
-        for (; i < values.size(); ++i)
-        {
-            result += values[i];
-        }
-
-        alignas(vector<T, simd_tag>::required_alignment) T result_values[vector<T, simd_tag>::capacity];
-        simd_results[0].store_p(result_values);
-        for (const T r : result_values)
-        {
-            result += r;
-        }
+        result += horizontal_add(simd_results[0]);
 
         return result;
     }
