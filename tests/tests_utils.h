7e9c24fed9a433ee09f7ea0e2d26fca8cb06f0a8
Nikita Ivanov <ramzesxi@gmail.com>
2018-09-15 18:55:54 +0300

    extra log info for skipped tests

--- tests/tests_utils.h
+++ tests/tests_utils.h
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 
-#include <iostream>
+#include <cstdio>
 
 template<typename S, typename V>
 struct TypePair
@@ -13,13 +13,23 @@ struct TypePair
     using value_type = V;
 };
 
-#define SKIP_UNAVAILABLE_TEST(simd_tag)                                                     \
-    if constexpr (simd::is_simd_tag<simd_tag>())                                            \
-    {                                                                                       \
-        if (not simd::has_simd_instrset<simd_tag>())                                        \
-        {                                                                                   \
-            std::cout << "WARNING: Instr set is not available, skip test!" << std::endl;    \
-            SUCCEED();                                                                      \
-            return;                                                                         \
-        }                                                                                   \
+#define SKIP_UNAVAILABLE_TEST(simd_tag)                                         \
+    if constexpr (simd::is_simd_tag<simd_tag>())                                \
+    {                                                                           \
+        if (not simd::has_simd_instrset<simd_tag>())                            \
+        {                                                                       \
+            const ::testing::TestInfo* const test_info =                        \
+                ::testing::UnitTest::GetInstance()->current_test_info();        \
+            std::printf                                                         \
+            (                                                                   \
+                "WARNING: Instr set is not available, skip test %s:%s:%s! \n"   \
+                , test_info->test_case_name()                                   \
+                , test_info->name()                                             \
+                , test_info->type_param() != nullptr                            \
+                    ? test_info->type_param()                                   \
+                    : ""                                                        \
+            );                                                                  \
+            SUCCEED();                                                          \
+            return;                                                             \
+        }                                                                       \
     }
